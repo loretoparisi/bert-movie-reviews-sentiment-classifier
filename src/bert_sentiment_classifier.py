b74ec8bf2e96aae6a83c87b2c7cacbff44f52181
Loreto Parisi <loretoparisi@gmail.com>
2019-04-06 10:42:55 +0200

    Updated notebook code

--- src/bert_sentiment_classifier.py
+++ src/bert_sentiment_classifier.py
@@ -1,7 +1,7 @@
 #%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
 import os
 try:
-	os.chdir(os.path.join(os.getcwd(), 'lib'))
+	os.chdir(os.path.join(os.getcwd(), 'src'))
 	print(os.getcwd())
 except:
 	pass
